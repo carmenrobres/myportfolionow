75370614914fe09675a10dfb8949654f7ce2ae0b
carmenrobres <crdv023@gmail.com>
2026-02-15 20:07:15 +0100

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/myportfolio/', // 👈 change this to match your GitHub repo name exactly
+    base: '/myportfolionow/', // 👈 change this to match your GitHub repo name exactly
     server: {
       port: 3000,
       host: '0.0.0.0',
